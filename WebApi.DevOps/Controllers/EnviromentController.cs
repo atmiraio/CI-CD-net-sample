a3798586bd3669cd86c3217293622d562c48f92b
aitor.garcia <aitor.garcia@atmira.com>
2021-12-03 12:06:43 +0100

    Add proyect for build

---
+++ WebApi.DevOps/Controllers/EnviromentController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace WebApi.DevOps.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EnviromentController : ControllerBase
+    {
+        private readonly ILogger<EnviromentController> _logger;
+        private readonly IConfiguration _config;
+
+        public EnviromentController(ILogger<EnviromentController> logger, IConfiguration config)
+        {
+            _logger = logger;
+            _config = config;
+        }
+
+        [HttpGet("getEnviroments")]
+        public IEnumerable<Enviroment> GetEnviroments()
+        {
+            _logger.LogDebug("Call GetEnviroments");
+
+            var enviroments = new List<Enviroment>();
+            enviroments.Add(new Enviroment(1, "Development", "DEV"));
+            enviroments.Add(new Enviroment(2, "Integration", "INT"));
+            enviroments.Add(new Enviroment(3, "Preproduction", "PRE"));
+            enviroments.Add(new Enviroment(4, "Production", "PRO"));
+            return enviroments;
+        }
+
+        [HttpGet("checkEnviroment")]
+        public string CheckEnviroment()
+        {  _logger.LogDebug("Call CheckEnviroment");
+
+            return $"The active enviroment is {_config["Config:Enviroment"]}";
+          
+        }
+    }
+}
---
+++ WebApi.DevOps/Models/Enviroment.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApi.DevOps
+{
+    public class Enviroment
+    {
+        public int EnviromentId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public Enviroment(int enviromentId, string name, string description)
+        {
+            EnviromentId = enviromentId;
+            Name = name;
+            Description = description;
+        }
+    }
+}
