eefd8514ff8d774f38095ed00e302bf5521ce55e
aitor.garcia <aitor.garcia@atmira.com>
2021-12-03 12:10:43 +0100

    Add Unit Test proyect

---
+++ WebApi.DevOps.Test/EnviromentControllerTest.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+using WebApi.DevOps.Controllers;
+
+namespace WebApi.DevOps.Test
+{
+    public class EnviromentControllerTest
+    {
+        private Mock<ILogger<EnviromentController>> _mockLogger;
+        private Mock<IConfiguration> _mockConfig;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockLogger = new Mock<ILogger<EnviromentController>>();
+            _mockConfig = new Mock<IConfiguration>();
+            
+        }
+
+        [Test]
+        public void GetEnviromentsTest()
+        {
+            EnviromentController controller = new EnviromentController(_mockLogger.Object, _mockConfig.Object);
+            var result = controller.GetEnviroments();
+
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void CheckEnviromentTest()
+        {
+            _mockConfig.SetupGet(m => m[It.Is<string>(s => s == "Config:Enviroment")]).Returns("Testing");
+            EnviromentController controller = new EnviromentController(_mockLogger.Object, _mockConfig.Object);
+            var result = controller.CheckEnviroment();
+
+            Assert.AreEqual("The active enviroment is Testing", result);
+        }
+    }
+}
